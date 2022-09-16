8792e2af8aafc45783b3ec8b3a4aef8efe0730a6
Nirmel Murtic <nmurtic@apple.com>
2022-09-17 00:18:39 +0200

    - adjustment

--- fast-api-service/app/wiki/schemas.py
+++ fast-api-service/app/wiki/schemas.py
@@ -1,15 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
-
-
-class WikiMeta(BaseModel):
-    uri: str
-
 
 class WikiRecord(BaseModel):
-    id: Optional[int]
     wiki: str
     title: str
-    user: str
-    comment: str
-    meta: WikiMeta

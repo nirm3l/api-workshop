833524b6f4805b5d300906c9d1fcbb06e14d9771
Nirmel Murtic <nmurtic@apple.com>
2022-10-16 04:24:15 +0200

    - updates

--- node-service/index.js
+++ node-service/index.js
@@ -8,7 +8,7 @@ app.use(actuator({
     basePath: '/actuator'
 }));
 
-const PORT = 8080;
+const PORT = 8084;
 
 const URL = 'https://stream.wikimedia.org/v2/stream/recentchange';
 const eventSource = new EventSource(URL);
